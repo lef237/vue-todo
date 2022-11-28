51b5a11656abcb8cc390de196edd30b2caf3249b
lef237 <lef237k5@gmail.com>
2022-11-28 16:38:47 +0900

    編集できるようにした。

--- main.js
+++ main.js
@@ -10,14 +10,22 @@ const app = Vue.createApp({
       if (this.newTask === "") return;
       let todo = {
         task: this.newTask,
-        isDone: false
+        isDone: false,
+        isEditing: false,
       };
       this.todos.push(todo);
       this.newTask = "";
     },
-    deleteTask: function(index) {
-      this.todos.splice(index, 1)
-    }
+    deleteTask: function (index) {
+      this.todos.splice(index, 1);
+    },
+    updateTask(index) {
+      this.todos[index].isEditing = true;
+      // this.todos[index].task
+    },
+    updateDone(index) {
+      this.todos[index].isEditing = false;
+    },
   },
 });
 app.mount("#app");
